d638260b81b5fabd61fb2b3658fea0e7af928049
3075559W <3075559W@student.gla.ac.uk>
2026-03-11 05:57:16 +0800

    chore: register accounts app in settings and setup main urls

--- glatrade/urls.py
+++ glatrade/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('market.urls')),
+    # hook up the accounts app routes
+    path('accounts/', include('accounts.urls')), 
 ]
