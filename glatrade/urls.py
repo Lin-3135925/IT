c2bd7d8b73c78ddb682de0856978dd8638f749d6
Chongqian Lin <3135925L@student.gla.ac.uk>
2026-03-12 20:37:12 +0800

    Fix: wire market urls in project routing

--- glatrade/urls.py
+++ glatrade/urls.py
@@ -1,15 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.http import HttpResponse
-
-# temporary view for testing the login redirect
-def temp_browse(request):
-    return HttpResponse("<h1>🎉 Welcome to the Browse Page! You are successfully logged in!</h1>")
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
-    
-    # map the root URL to our temporary browse view
-    path('', temp_browse, name='browse'), 
+    path("admin/", admin.site.urls),
+    path("accounts/", include("accounts.urls")),
+    path("", include("market.urls")),
 ]
